6a78a7e3f770e80ee12f934260494e939456c480
Michael Kornelakis <kornelakism@gmail.com>
2025-12-25 09:33:12 +0200

    convert to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone: true,
+    imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'photo-library';

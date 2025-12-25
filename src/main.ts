936a37d5027dcb4085eaad8e77015a59a12105dd
Michael Kornelakis <kornelakism@gmail.com>
2025-12-25 09:56:11 +0200

    add angular material

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule), provideRouter(routes)]
+  providers: [importProvidersFrom(BrowserModule), provideRouter(routes), provideAnimations()]
 })
   .catch(err => console.error(err));

789a9d2485e183a23f411a90614b56fd15ed4348
Michael Kornelakis <kornelakism@gmail.com>
2025-12-25 11:52:39 +0200

    refactor the test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule, AppComponent]
-  }));
-  it('should render title', () => {
+  beforeEach(async () => await TestBed.configureTestingModule({
+    imports: [AppComponent],
+  }).compileComponents());
+
+  it('should render title', async () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Photo Library');
+
+    expect(compiled.querySelector('h1')?.textContent)
+      .toContain('Photo Library');
   });
 });

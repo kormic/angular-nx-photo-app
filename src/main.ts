30013be4674fcad8aa644947671b062d89d7e083
Michael Kornelakis <kornelakism@gmail.com>
2025-12-25 10:34:55 +0200

    remove the browser module and use app config

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 
 
-bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule), provideRouter(routes), provideAnimations()]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));

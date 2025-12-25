1de6500a9386a42d5aa0ea3be5a0997b9c941a57
Michael Kornelakis <kornelakism@gmail.com>
2025-12-25 09:52:08 +0200

    remove routing module and use provideRouter

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+  providers: [importProvidersFrom(BrowserModule), provideRouter(routes)]
 })
   .catch(err => console.error(err));

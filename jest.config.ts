7adb8a7f8d6077d489a99a6d5cedb1114442b274
Michael Kornelakis <kornelakism@gmail.com>
2025-12-26 02:52:22 +0200

    use jest and remove jasmine and karma

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  displayName: 'photo-library',
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.html$',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'html', 'js'],
+  testMatch: ['**/?(*.)+(spec).ts'],
+  coverageDirectory: 'coverage',
+};

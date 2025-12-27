5772bb0b51511cf70ffa97424bbb6a1eaee0f877
Michael Kornelakis <kornelakism@gmail.com>
2025-12-27 14:27:10 +0200

    add a temp navbar

--- apps/photo-gallery/src/app/app.component.ts
+++ apps/photo-gallery/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
 })
 export class AppComponent {
   title = 'Photo Library';

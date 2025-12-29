146339871b2d6c2d622790d3a64ae523fa867772
Michael Kornelakis <kornelakism@gmail.com>
2025-12-29 15:04:09 +0200

    add the photo-card and cleanup shared

---
+++ libs/shared/src/lib/shared/ui/photo-card/photo-card.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'lib-photo-card',
+  styles: ``,
+  template: `
+    <mat-card class="example-card" appearance="outlined">
+      <img
+        mat-card-image
+        src="https://material.angular.dev/assets/img/examples/shiba2.jpg"
+        alt="Photo of a Shiba Inu" />
+      <mat-card-actions>
+        <button matButton>LIKE</button>
+      </mat-card-actions>
+    </mat-card>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [MatCardModule, MatButtonModule],
+})
+export class PhotoCard {}
